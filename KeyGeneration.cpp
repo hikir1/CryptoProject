c40717e160c385ae58ba1c9c7459d9483e38c408
dsp125 <dpatel0125@gmail.com>
2020-08-10 21:39:34 -0400

    pseudocode for diffiehellman added

--- KeyGeneration.cpp
+++ KeyGeneration.cpp
@@ -47,53 +47,49 @@ void sharedkey(mpz_t sharedSecret, mpz_t receivedkeyhalf, mpz_t p, mpz_t pkb){
 /*
 	input: sendingkeyhalf - empty initialized mpz_t 
 */
-
-/* testing code for diffie hellman
-void KeyExchange(mpz_t sendingkeyhalf)
+void KeyExchange(mpz_t sendingkeyhalf, mpz_t p, mpz_t pkb)
 {
 	int alg;
-	std::cout << "Choose an algorithm\n";
-	std::cout << "Enter [1] to input your own private key\nEnter [2] for randomly generated private key\n\n";
-	std::cin >> alg;
-	if (alg == 1) {
-		//establish public keys
-		mpz_t p; 
-		mpz_t g;
-		mpz_init(p);
-		mpz_init(g);
-		//getRandPrime(p);
-		//getRandPrime(g);
-		mpz_set_ui(p,23);
-		mpz_set_ui(g,9);
-		std::cout << "P: " << p <<std::endl << "G: " << g <<std::endl;
-		//enter private key
-		std::string priv;
-		std::cout << "Enter your private key: "<< std::endl;
-		std::cin >> priv;
-		mpz_t pkb;
-		mpz_init(pkb);
-		const char* pk = priv.c_str();
-		mpz_set_str(pkb,pk,10);
-		std::cout << "Your key: "<< pkb <<std::endl;
-		sendKeyDiffieHellman(sendingkeyhalf,g,p,pkb);
-		mpz_clear(p);
-		mpz_clear(g);
-		std::cout << "Sending Key Half: " << sendingkeyhalf <<std::endl;
-	}
-	else if (alg == 2) {
-		//establish public keys
-		//this will be done by server
-		mpz_t p; 
-		mpz_t g;
-		mpz_init(p);
-		mpz_init(g);
-		getRandPrime(p);
-		getRandPrime(g);
-		sendKeyDiffieHellman(sendingkeyhalf,g,p);
-	}
-	else {
-		std::cout << "Error" <<std::endl <<std::flush;
-		std::cerr << "Invalid Algorithm Option: " << alg <<std::endl;
-	}
+	//establish public keys
+	mpz_t g;
+	mpz_init(g);
+	getRandPrime(p);
+	getRandPrime(g);
+	std::string priv;
+	std::cout << "Enter your private key: "<< std::endl;
+	std::cin >> priv;
+	const char* pk = priv.c_str();
+	mpz_set_str(pkb,pk,10);
+	std::cout << "Your key: "<< pkb <<std::endl;
+	sendKeyDiffieHellman(sendingkeyhalf,g,p,pkb);
+	mpz_clear(g);
 }
-*/
+
+/* How to use code above
+
+//initialize 3 mpz_t types
+mpz_t keyhalf;
+mpz_init(keyhalf);
+
+mpz_t p;
+mpz_init(p);
+
+mpz_t pkb;
+mpz_init(pkb);
+
+mpz_t other_keyhalf;
+mpz_init(other_keyhalf);
+
+mpz_t shared_key;
+mpz_init(shared_key);
+
+KeyExchange(keyhalf); //keyhalf has proper values after this
+//send keyhalf here
+
+//receive key as char*
+mpz_set_str(received char*, other_key_half, 10) // this converts char* to key half
+
+sharedkey(shared_key, other_key, p, pkb) //stores shared key in shared_key after this
+
+//mpz_t is a pointer, run mpz_clear(mpt_t variablename) to free the memory
+/
