daa690a096e5bf836a1c30e37c36bb2a772bc6e4
nathanWhitney <nmwhitney.cs@gmail.com>
2020-08-11 13:40:32 -0400

    fixing

--- ssh.hpp
+++ ssh.hpp
@@ -9,6 +9,7 @@ constexpr const char * HELLO_MSG = "I AM NOT A HACKER";
 constexpr size_t HELLO_LEN = strlen(HELLO_MSG) + 1;
 static_assert(HELLO_LEN <= MSG_MAX);
 
+[[deprecated]] // define in diffie helman header
 constexpr size_t PARKEY_LEN = 16;
 
 constexpr size_t KEYEX_LEN = 3 * PARKEY_LEN;
