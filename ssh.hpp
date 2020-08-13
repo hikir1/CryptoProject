133a40338b9e5fc22a70c49f30fd3dd48678b7f6
hikir1 <hikirone@gmail.com>
2020-08-13 10:19:24 -0400

    server should work now

--- ssh.hpp
+++ ssh.hpp
@@ -24,8 +24,10 @@ constexpr size_t HELLO_LEN = strlen(HELLO_MSG) + 1; // + 1 for null byte
 constexpr size_t CLIENT_PTXT_KEYEX_LEN = 6 * KeyGen::diffiekeyhalfsize;
 constexpr size_t SERVER_PTXT_KEYEX_LEN = 2 * KeyGen::diffiekeyhalfsize;
 
-constexpr size_t CLIENT_KEYEX_LEN = 
-constexpr size_t SERVER_KEYEX_LEN = 
+constexpr size_t CLIENT_KEYEX_LEN = CLIENT_PTXT_KEYEX_LEN
+		+ (CLIENT_PTXT_KEYEX_LEN % RSA_MAX > 0? RSA_MAX - CLIENT_PTXT_KEYEX_LEN % RSA_MAX: 0);
+constexpr size_t SERVER_KEYEX_LEN = SERVER_PTXT_KEYEX_LEN
+		+ (SERVER_PTXT_KEYEX_LEN % RSA_MAX >0? RSA_MAX - CLIENT_PTXT_KEYEX_LEN % RSA_MAX: 0)
 
 static_assert(HELLO_LEN <= RECV_MAX);
 static_assert(CLIENT_KEYEX_LEN <= RECV_MAX);
@@ -43,16 +45,20 @@ int genKeys(std::string hmac_shared, std::string aes_shared, ssh::Keys &keys);
 class ClientDiffieKeys {	
 	std::vector<std::string> hmac_keys;
 	std::vector<std::string> aes_keys;
-	char buf[CLIENT_KEYEX_LEN];
+	char buf[CLIENT_KEYEX_LEN] = {0};
 	public:
-	ClientDiffieKeys() : hmac_keys(KeyGen::createKeyhalf_client()),
+	ClientDiffieKeys(RSA &rsa) : hmac_keys(KeyGen::createKeyhalf_client()),
 			aes_keys(KeyGen::createKeyhalf_client()) {
 		assert(hmac_keys[0].size() == KeyGen::diffiekeyhalfsize);
 		assert(aes_keys[0].size() == KeyGen::diffiekeyhalfsize);
-		for (int i = 0; i < 3; i++)
-			memcpy(buf + i * KeyGen::diffiekeyhalfsize, hmac_keys[i].data(), KeyGen::diffiekeyhalfsize);
-		for (int i = 0; i < 3; i++)
-			memcpy(buf + (i+3) * KeyGen::diffiekeyhalfsize, aes_keys[i].data(), KeyGen::diffiekeyhalfsize);
+		std::string ptxt1 = hmac_keys[0] + hmac_keys[1];
+		std::string ptxt2 = hmac_keys[2] + aes_keys[0];
+		std::string ptxt3 = aes_keys[1] + aes_keys[2];
+		std::string ctxt = rsa.RSAgetcryptotext(ptxt1);
+		ctxt += rsa.RSAgetcryptotext(ptxt2);
+		ctxt += rsa.RSAgetcryptotext(ptxt3);
+		assert(ctxt.size() == CLIENT_KEYEX_LEN);
+		memset(buf, ctxt.data(), CLIENT_KEYEX_LEN);
 	}
 	const char * pubKeys() const {
 		return buf;
@@ -65,22 +71,32 @@ class ServerDiffieKeys {
 	std::vector<std::string> aes_keys;
 	std::string hmac_shared;
 	std::string aes_shared;
-	char buf[SERVER_KEYEX_LEN];
+	char buf[SERVER_KEYEX_LEN] = {0};
 	public:
-	ServerDiffieKeys(const char keyex_recv[CLIENT_KEYEX_LEN]) {
+	ServerDiffieKeys(const char keyex_recv[CLIENT_KEYEX_LEN], RSA &rsa) {
+		std::string ctxt1 = std::string(keyex_recv, RSA_MAX);
+		std::string ctxt2 = std::string(keyex_recv + RSA_MAX, RSA_MAX);
+		std::string ctxt3 = std::string(keyex_recv + 2*RSA_MAX, RSA_MAX);
+		std::string ptxt1 = rsa.RSAgetmessage(ctxt1);
+		std::string ptxt2 = rsa.RSAgetmessage(ctxt2);
+		std::string ptxt3 = rsa.RSAgetmessage(ctxt3);
 		std::vector<std::string> temp_hmac, temp_aes;
-		for (int i = 0; i < 3; i++)
-			temp_hmac.push_back(std::string(keyex_recv + i * KeyGen::diffiekeyhalfsize,
-					KeyGen::diffiekeyhalfsize));
-		for (int i = 0; i < 3; i++)
-			temp_aes.push_back(std::string(keyex_recv + (i+3) * KeyGen::diffiekeyhalfsize,
-					KeyGen::diffiekeyhalfsize));
+		temp_hmac.push_back(ptxt1.substr(0, KeyGen::diffiekeyhalfsize));
+		temp_hmac.push_back(ptxt1.substr(KeyGen::diffiekeyhalfsize));
+		temp_hmac.push_back(ptxt2.substr(0, KeyGen::diffiekeyhalfsize));
+		temp_aes.push_back(ptxt2.substr(KeyGen::diffiekeyhalfsize));
+		temp_aes.push_back(ptxt3.substr(0, KeyGen::diffiekeyhalfsize));
+		temp_aes.push_back(ptxt3.substr(KeyGen::diffiekeyhalfsize));
+		
 		hmac_keys = KeyGen::createKeyhalf_server(temp_hmac[1], temp_hmac[2]);
 		aes_keys = KeyGen::createKeyhalf_server(temp_aes[1], temp_aes[2]);
 		assert(hmac_keys[0].size() == KeyGen::diffiekeyhalfsize);
 		assert(aes_keys[0].size() == KeyGen::diffiekeyhalfsize);
-		memcpy(buf, hmac_keys[0].data(), KeyGen::diffiekeyhalfsize);
-		memcpy(buf + KeyGen::diffiekeyhalfsize, aes_keys[0].data(), KeyGen::diffiekeyhalfsize);
+
+		std::string send_ctxt = rsa.getcryptotext(hmac_keys[0] + aes_keys[0]);
+		assert(send_ctxt.size() == SERVER_KEYEX_LEN);
+		memcpy(buf, send_ctxt.data(), send_ctxt.size());
+
 		hmac_shared = KeyGen::getSharedKey(hmac_keys, temp_hmac[0]);
 		aes_shared = KeyGen::getSharedKey(aes_keys, temp_aes[0]);
 	}
